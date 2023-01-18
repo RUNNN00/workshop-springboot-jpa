3d550f92aced03cb2dd242b37ce7b60fe683d98c
ruanazevedo <ruann.azevedo@outlook.com>
2023-01-17 21:29:37 -0300

    service layer, component registration

---
+++ src/main/java/com/webservicedemo/demo/services/UserService.java
@@ -0,0 +1,26 @@
+package com.webservicedemo.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webservicedemo.demo.entities.User;
+import com.webservicedemo.demo.repositories.UserRepository;
+
+@Service // anota essa classe como component para usa-la com Autowired
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
