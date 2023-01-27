92e33eeca0749e73d05c02d76387a88cb00cea95
ruanazevedo <ruannzeroh@gmail.com>
2023-01-27 13:02:59 -0300

    payment, one-to-one association

--- src/main/java/com/webservicedemo/demo/config/TestConfig.java
+++ src/main/java/com/webservicedemo/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.webservicedemo.demo.entities.Category;
 import com.webservicedemo.demo.entities.Order;
 import com.webservicedemo.demo.entities.OrderItem;
+import com.webservicedemo.demo.entities.Payment;
 import com.webservicedemo.demo.entities.Product;
 import com.webservicedemo.demo.entities.User;
 import com.webservicedemo.demo.entities.enums.OrderStatus;
@@ -71,6 +72,9 @@ public class TestConfig implements CommandLineRunner {
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
